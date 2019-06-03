c2b5586c28f46d25127e6a21c5cbae029fdb567e
mingchaotang <mingchao.tang@gmail.com>
2019-06-03 15:23:17 -0400

    creating a responsive SideDrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -7,7 +7,6 @@ import Toolbar from '../Navigation/Toolbar/Toolbar';
 const layout = props => (
   <Aux>
     <Toolbar />
-    <div>Toolbar, SideDrawer, Backdrop</div>
     <main className={classes.Content}>{props.children}</main>
   </Aux>
 );
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -3,12 +3,13 @@ import React from 'react';
 import classes from './Toolbar.css';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import SideDrawer from '../SideDrawer/SideDrawer';
 
 const toolbar = props => (
   <header className={classes.Toolbar}>
-    <div>MENU</div>
+    <SideDrawer />
     <Logo />
-    <NavigationItems/>
+    <NavigationItems />
   </header>
 );
 
