9b79bd35460dc931a8a6318dd914509b341ce76b
mingchaotang <mingchao.tang@gmail.com>
2019-06-03 15:55:24 -0400

    working on responsive adjustments

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -3,12 +3,13 @@ import React from 'react';
 import classes from './Toolbar.css';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
-import SideDrawer from '../SideDrawer/SideDrawer';
 
 const toolbar = props => (
   <header className={classes.Toolbar}>
-    <SideDrawer />
-    <Logo />
+    <div>MENU</div>
+    <div className={classes.Logo}>
+      <Logo />
+    </div>
     <NavigationItems />
   </header>
 );
