d96e9deca2c1fe43560b635128f00f3a1759494d
mingchaotang <mingchao.tang@gmail.com>
2019-06-13 16:48:30 -0400

    building the checkout container

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Checkout from './containers/Checkout/Checkout';
 
 class App extends Component {
   // Comment code is used for checking interceptors removal
@@ -21,6 +22,7 @@ class App extends Component {
         {/*<Layout>{this.state.show ? <BurgerBuilder /> : null}</Layout>*/}
         <Layout>
           <BurgerBuilder />
+          <Checkout />
         </Layout>
       </div>
     );
