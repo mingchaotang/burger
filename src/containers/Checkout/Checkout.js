17c9cb21659c96c017e576129652d52f0d8bb561
mingchaotang <mingchao.tang@gmail.com>
2019-07-09 12:17:24 -0400

    redirect to improve UX

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,8 +1,9 @@
-import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
-import { connect } from 'react-redux';
+import React, {Component} from 'react';
+import {Route, Redirect} from 'react-router-dom';
+import {connect} from 'react-redux';
 
-import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckouSummary';
+import CheckoutSummary
+  from '../../components/Order/CheckoutSummary/CheckouSummary';
 import ContactData from './ContactData/ContactData';
 
 class Checkout extends Component {
@@ -15,26 +16,30 @@ class Checkout extends Component {
   };
 
   render() {
-    return (
-      <div>
-        <CheckoutSummary
-          ingredients={this.props.ings}
-          checkoutCancelled={this.checkoutCancelledHandler}
-          checkoutContinued={this.checkoutContinuedHandler}
-        />
-        <Route
-          path={this.props.match.path + '/contact-data'}
-          component={ContactData}
-        />
-      </div>
-    );
+    let summary = (<Redirect to="/"/>);
+    if (this.props.ings) {
+      summary = (
+          <div>
+            <CheckoutSummary
+                ingredients={this.props.ings}
+                checkoutCancelled={this.checkoutCancelledHandler}
+                checkoutContinued={this.checkoutContinuedHandler}
+            />
+            <Route
+                path={this.props.match.path + '/contact-data'}
+                component={ContactData}
+            />
+          </div>
+      );
+    }
+    return summary;
   }
 }
 
 const mapStateToProps = state => {
   return {
     ings: state.ingredients,
-    ttp: state.totalPrice
+    ttp: state.totalPrice,
   };
 };
 
