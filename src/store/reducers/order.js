0f7cc4edff73a85a196d7f8ab039e5e4964b343a
mingchaotang <mingchao.tang@gmail.com>
2019-07-10 11:43:37 -0400

    fix typo

--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -11,7 +11,7 @@ const purchaseInit = (state, action) => {
   return updateObject(state, { purchased: false });
 };
 
-const purchaseBurgerSucess = (state, action) => {
+const purchaseBurgerSuccess = (state, action) => {
   const newOrder = updateObject(action.orderData, { id: action.orderId });
   return updateObject(state, {
     orders: state.orders.concat(newOrder),
@@ -34,7 +34,7 @@ const fetchOrdersStart = (state, action) => {
 
 const fetchOrdersSuccess = (state, action) => {
   return updateObject(state, {
-    rders: action.orders,
+    orders: action.orders,
     loading: false
   });
 };
@@ -48,7 +48,7 @@ const reducer = (state = initialState, action) => {
     case actionTypes.PURCHASE_INIT:
       return purchaseInit(state, action);
     case actionTypes.PURCHASE_BURGER_SUCCESS:
-      return purchaseBurgerSucess(state, action);
+      return purchaseBurgerSuccess(state, action);
     case actionTypes.PURCHASE_BURGER_FAIL:
       return purchaseBurgerFail(state, action);
     case actionTypes.PURCHASE_BURGER_START:
