d84d0ee622a9176027677a02ed9c501dcab5d0f2
mingchaotang <mingchao.tang@gmail.com>
2019-07-15 16:04:46 -0400

    storing the token

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 import * as actionTypes from './actionTypes';
-import { deleteOrderFail, deleteOrderSuccess } from './order';
 
 export const authStart = () => {
   return {
@@ -9,10 +8,11 @@ export const authStart = () => {
   };
 };
 
-export const authSuccess = authData => {
+export const authSuccess = (token, userId) => {
   return {
     type: actionTypes.AUTH_SUCCESS,
-    authData: authData
+    idToken: token,
+    userId: userId
   };
 };
 
@@ -41,7 +41,7 @@ export const auth = (email, password, isSignup) => {
       .post(url, authData)
       .then(response => {
         console.log(response);
-        dispatch(authSuccess(response.data));
+        dispatch(authSuccess(response.data.idToken, response.data.localId));
       })
       .catch(error => {
         console.log(error);
