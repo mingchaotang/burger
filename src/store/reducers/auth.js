248bdd9fe3ce1458929a9b1aef9d9e875bbd7297
mingchaotang <mingchao.tang@gmail.com>
2019-07-16 10:41:17 -0400

    logging users out

--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -21,6 +21,13 @@ const authSuccess = (state, action) => {
   });
 };
 
+const authLogout = (state, action) => {
+  return updateObject(state, {
+    token: null,
+    userId: null
+  });
+};
+
 const authFail = (state, action) => {
   return updateObject(state, { error: action.error, loading: false });
 };
@@ -33,6 +40,8 @@ const reducer = (state = initialState, action) => {
       return authSuccess(state, action);
     case actionTypes.AUTH_FAIL:
       return authFail(state, action);
+    case actionTypes.AUTH_LOGOUT:
+      return authLogout(state, action);
     default:
       return state;
   }
